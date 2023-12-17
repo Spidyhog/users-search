93e89d0472e6f8410e240d1d20656e5a05e02fcc
priyanshu <priyanshu.m@gigindia.in>
2023-12-17 21:23:43 +0530

    updated

---
+++ out/_next/static/8zb9lZcJT5s9gtoLt5cFg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/871-94904abfa524b0c8.js","static/css/2b3c4dae30c26331.css","static/chunks/pages/index-1c5c615582db60c9.js"],"/_error":["static/chunks/pages/_error-b6491f42fb2263bb.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
